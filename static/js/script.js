fbdb2a58e06754bbcb17a0fb7188de9ea6c5c47d
bradleykenny <bkenny283@gmail.com>
2018-05-09 14:56:56 +1000

    Initial commit

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+(function(){
+
+    /* your code goes here */
+
+    $(document).ready(function(){
+        $("#content").text("This text was placed here by the javascript in static/js/script.js")
+        })
+})()
