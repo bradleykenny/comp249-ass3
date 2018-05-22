bcf26cca82d0c9347865639ab3d6056cede79d1d
bradleykenny <bkenny283@gmail.com>
2018-05-22 22:30:48 +1000

    Can see JSON data on page now.

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,17 @@
     /* your code goes here */
 
     $(document).ready(function(){
-        $("#content").text("This text was placed here by the javascript in static/js/script.js")
+        // $("#content").text("This text was placed here by the javascript in static/js/script.js")
+
+		$.getJSON('/positions', function(data) {
+			console.log(data);
+			for (var i = 0; i < 10; i++) {
+				var output = "<div class='card'>"
+				output += data[i].title;
+				output += "<br>" + data[i].created_at;
+				output += "</div>"
+				$("#content").append(output);
+			}
+    	});
     })
 })()
