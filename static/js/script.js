3ac5ccd935a76d0498001b0e63a053a8c71d4b55
bradleykenny <bkenny283@gmail.com>
2018-05-23 14:09:32 +1000

    Style improvements and implemented .js methods.

--- static/js/script.js
+++ static/js/script.js
@@ -9,8 +9,10 @@
 			console.log(data);
 			for (var i = 0; i < 10; i++) {
 				var output = "<div class='card'>"
-				output += data[i].title;
-				output += "<br>" + data[i].created_at;
+				output += "<div class='jobtitle'>" + data[i].title + "</div>";
+				output += "<div class='company'><img src='static/img/company.png/'>" + data[i].company + "</div>";
+				output += "<div class='location'><img src='static/img/location.png/'>" + data[i].location + "</div>";
+				output += "<div class='time'><img src='static/img/time.png/'>" + data[i].created_at + "</div>";
 				output += "</div>"
 				$("#content").append(output);
 			}
