1e98f40acd1a09f20e5edc002f6aefcd27d209d2
bradleykenny <bkenny283@gmail.com>
2018-05-31 07:14:29 +1000

    Can click title for 'read more' function now, too.

--- static/js/content.js
+++ static/js/content.js
@@ -1,24 +1,25 @@
 (function(){
 
-    /* your code goes here */
-
     $(document).ready(function(){
         // $("#content").text("This text was placed here by the javascript in static/js/script.js")
 		$.getJSON('/positions', function(data) {
 			console.log(data);
 			for (var i = 0; i < 10; i++) {
 				// creates and adds to a string that outputs html into the cards
-				var output = "<div class='card'>"
+				var output = "<div class='card' id='" + i + "'>"
 				output += "<div class='jobtitle'>" + data[i].title + "</div>";
 				output += "<div class='company'><img src='static/img/company.png/'>" + data[i].company + "</div>";
 				output += "<div class='location'><img src='static/img/location.png/'>" + data[i].location + "</div>";
 				output += "<div class='time'><img src='static/img/time.png/'>" + data[i].created_at + "</div>";
 				// output += "<hr><div class='description'>" + data[i].description + "</div>";
-				output += "<a class='buttonBottomSubmerged' id='" + i + "'>Read More</a>"
+				output += "<a class='buttonBottomSubmerged'>Read More</a>"
 				output += "</div>";
 				$("#preview_cards").append(output);
+				$(".jobtitle").click(function() {
+					mainCardContent($(this).parent().attr('id'));
+				})
 				$(".buttonBottomSubmerged").click(function() {
-					mainCardContent(this.id);
+					mainCardContent($(this).parent().attr('id'));
 				})
 			}
 			mainCardContent(0);
