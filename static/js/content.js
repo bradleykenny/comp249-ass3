5f88d8c6d4a1a2d097aa632530c0b0d756ae0e7c
bradleykenny <bkenny283@gmail.com>
2018-05-31 11:18:27 +1000

    Implemented and designed the Apply card.

--- static/js/content.js
+++ static/js/content.js
@@ -1,6 +1,6 @@
 (function(){
 
-    $(document).ready(function(){
+    $(document).ready(function() {
         // $("#content").text("This text was placed here by the javascript in static/js/script.js")
 		$.getJSON('/positions', function(data) {
 			console.log(data);
@@ -29,20 +29,37 @@
 
 function mainCardContent(val) {
 	$.getJSON('/positions', function(data) {
-		var output = "<div class='card' id='contentCard'>"
-		output += "<div class='jobtitle'>" + data[val].title + "</div>";
+		$("#contentCard").show();
+		var output = "<div class='jobtitle'>" + data[val].title + "</div>";
 		output += "<div class='closeBox'><img src='static/img/close_32.png' id='closeBoxImg'/></div>";
 		output += "<div class='company'><img src='static/img/company.png/'>" + data[val].company + "</div>";
 		output += "<div class='location'><img src='static/img/location.png/'>" + data[val].location + "</div>";
 		output += "<div class='time'><img src='static/img/time.png/'>" + data[val].created_at + "</div>";
+		output += "<div class='button applyBtn'>Apply</div>"
 		output += "<hr><div class='description'>" + data[val].description + "</div>";
-		output += "<a class='buttonBottomSubmerged' id='" + val + "'>Apply Now</a>"
+		output += "<a class='buttonBottomSubmerged applyBtn' id='" + val + "'>Apply Now</a>"
 		// output += "<a class='button'>Read More</a>"
-		output += "</div>";
-		$("#content").html(output);
+		$("#contentCard").html(output);
 
 		$(".closeBox").click(function() {
 			$("#contentCard").hide()
 		})
+
+		$(".applyBtn").click(function() {
+			$("#applyCard").show();
+			var job_id = data[val].id;
+			$('input[name$=position_id]').val(job_id);
+			window.scrollTo(0,0);
+		})
+
+		$(".closeBoxApply").click(function() {
+			$("#applyCard").hide()
+		})
 	})
+}
+
+function applyCardContent(val) {
+	var output = "<div class='card' id='contentCard'>";
+	output += "<input type='text'/>";
+	output += "</div>";
 }
